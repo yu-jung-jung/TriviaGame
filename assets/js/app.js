4cca6f83be1faad944822288443ed8d6cadd5af3
YU JUNG JUNG <i.respectyou.alot@gmail.com>
2017-09-19 19:46:44 -0400

    master

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+document.getElementById('timer').innerHTML =
+  04 + ":" + 00;
+
+startTimer();
+
+function startTimer() {
+
+  var presentTime = document.getElementById('timer').innerHTML;
+
+  var timeArray = presentTime.split(/[:]+/);
+
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+
+  if(m<0){alert('time out')}
+  
+  document.getElementById('timer').innerHTML = m + ":" + s;
+
+  setTimeout(startTimer, 1000);
+}
+
+// function checkSecond(sec) {
+
+//   if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+//   if (sec < 0) {sec = "59"};
+//   return sec;
+// }
+
